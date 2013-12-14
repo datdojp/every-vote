4fd6183847060e397116f677869045d8953b1639
datdvt@gmail.com <datdvt@gmail.com@c6f34d22-d4af-648e-959e-23dac6a6b504>
2013-12-14 12:27:31 +0000

    [Server] draft db design

--- server/every-vote/app/models/user.rb
+++ server/every-vote/app/models/user.rb
@@ -1,10 +1,19 @@
 class User
   include Mongoid::Document
-
-  field :user_name, type: String
-
-  embeds_many :sns_profiles, class_name: UserProfile.name, inverse_of: :user
-  has_many :requests, class_name: Request.name, inverse_of: :user
+  
+  # data
+  field :gcm_token, type: String
+  field :default_service, type: String
+  
+  # relations
+  embeds_many :profiles, class_name: UserProfile.name, inverse_of: :user
+  
+  has_many :requests, class_name: Request.name, inverse_of: :sender
+  has_and_belongs_to_many :in_requests, class_name: Request.name, inverse_of: :target_users
+  
   has_many :groups, class_name: Group.name, inverse_of: :user
   has_and_belongs_to_many :in_groups, class_name: Group.name, inverse_of: :group_users
+  
+  has_many :friends, class_name: Friend.name, inverse_of: :user
+  has_many :in_friends, class_name: Friend.name, inverse_of: :friend_user
 end
--- server/every-vote/app/models/user_profile.rb
+++ server/every-vote/app/models/user_profile.rb
@@ -1,10 +1,12 @@
 class UserProfile
   include Mongoid::Document
 
+  # data
   field :service, type: String
   field :user_name, type: String
   field :access_token, type: String
   field :refresh_token, type: String
 
-  embedded_in :user, class_name: User.name, inverse_of: :sns_profiles
+  # relations
+  embedded_in :user, class_name: User.name, inverse_of: :profiles
 end
